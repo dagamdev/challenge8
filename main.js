9d3e930f115ac16033022b5618dfc97101a923e5
2DavidGarcia4 <gacub20ru@gmail.com>
2022-07-01 16:56:23 -0500

    Start

---
+++ main.js
@@ -0,0 +1,18 @@
+const iconBurger = document.querySelector("#icon__burger");
+const menu = document.querySelector("#menu");
+
+iconBurger.addEventListener("click", function () {
+    menu.classList.toggle("show__menu");
+});
+
+
+const list = document.querySelectorAll("#menu .list")
+
+list.forEach((item)=> {
+    item.addEventListener("click", ()=>{
+        list.forEach((itemTwo) => {
+            itemTwo.classList.remove("active")
+        })
+        item.classList.add("active")
+    })
+})
